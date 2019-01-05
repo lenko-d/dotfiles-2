16d3757f2a1ef802472164df02b448bf2d0c8cc7
Lenko Donchev <1954225+lenko-d@users.noreply.github.com>
2019-01-05 11:09:20 -0600

    Update general-config.el

--- .emacs.files/general-config.el
+++ .emacs.files/general-config.el
@@ -227,3 +227,61 @@
 (global-set-key (kbd "C-x e") 'helm-grep-do-git-grep)
 (global-set-key (kbd "C-x l") 'helm-ls-git-ls)
 (global-set-key (kbd "C-c i c") 'compilation-mode)
+(global-set-key (kbd "C-c i s") 'helm-multi-swoop-all)
+
+(require 'shell-switcher)
+(setq shell-switcher-mode t)
+
+;; set key for agenda
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+;;file to save todo items
+(setq org-agenda-files (quote ("/Users/donchevl/work/NOTES.org")))
+
+;;set priority range from A to C with default A
+(setq org-highest-priority ?A)
+(setq org-lowest-priority ?C)
+(setq org-default-priority ?A)
+
+;;set colours for priorities
+(setq org-priority-faces '((?A . (:foreground "#F0DFAF" :weight bold))
+                           (?B . (:foreground "LightSteelBlue"))
+                           (?C . (:foreground "OliveDrab"))))
+
+;;open agenda in current window
+(setq org-agenda-window-setup (quote current-window))
+
+;;capture todo items using C-c c t
+(define-key global-map (kbd "C-c c") 'org-capture)
+(setq org-capture-templates
+      '(("t" "todo" entry (file+headline "/Users/donchevl/work/NOTES.org" "Tasks")
+         "* TODO [#A] %?")))
+
+
+
+(setq tags-table-list '("/Users/donchevl/work/webcms/riverside-insights-webcms"))
+ ;;     `M-.' default use symbol under point as tagname
+ ;;     `C-uM-.' use pattern you typed as tagname
+(global-set-key "\M-." 'helm-etags-plus-select)
+;;list all visited tags
+(global-set-key "\M-*" 'helm-etags-plus-history)
+;;go back directly
+(global-set-key "\M-[" 'helm-etags-plus-history-go-back)
+;;go forward directly
+(global-set-key "\M-]" 'helm-etags-plus-history-go-forward)
+
+
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+(require 'ace-isearch)
+(global-ace-isearch-mode +1)
+
+(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
+(setq auto-mode-alist
+(append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
+
+
+
